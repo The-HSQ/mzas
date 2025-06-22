c5185f47bc132dec5f656ff407e73927507885ab
The-HSQ <huaali7860@gmail.com>
2025-06-22 14:44:52 +0500

    Our leadership team is commit

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -5,39 +5,39 @@ import CompanyValues from "./CompanyValues";
 import WorkProcess from "./WorkProcess";
 
 const About = () => {
-  return (
-    <div className="flex flex-col gap-10">
-      {/* Hero Section */}
-      <section className="flex flex-col gap-3 justify-center items-center">
-        <h1 className="text-4xl font-bold tracking-tight">About MZAS</h1>
-        <p className="text-lg text-[var(--color-text-muted)] max-w-3xl text-center">
-          We are a team of passionate professionals dedicated to transforming
-          businesses through innovative solutions and expert consulting. Our
-          commitment to excellence and innovation drives everything we do.
-        </p>
-      </section>
+	return (
+		<div className="flex flex-col gap-10">
+			{/* Hero Section */}
+			<section className="flex flex-col gap-3 justify-center items-center">
+				<h1 className="text-4xl font-bold tracking-tight">About MZAS</h1>
+				<p className="text-lg text-[var(--color-text-muted)] max-w-3xl text-center">
+					We are a team of passionate professionals dedicated to transforming
+					businesses through innovative solutions and expert consulting. Our
+					commitment to excellence and innovation drives everything we do.
+				</p>
+			</section>
 
-      <div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
+			<div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
 
-      {/* Company Story Section */}
-      <CompanyStory />
+			{/* Company Story Section */}
+			<CompanyStory />
 
-      <div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
+			<div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
 
-      {/* Leadership Team Section */}
-      <LeadershipTeam />
+			{/* Leadership Team Section */}
+			{/* <LeadershipTeam /> */}
 
-      <div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
+			<div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
 
-      {/* Values Section */}
-      <CompanyValues />
+			{/* Values Section */}
+			<CompanyValues />
 
-      <div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
+			<div className=" w-[280px] border-1 border-dashed border-[var(--color-border)] mx-auto"></div>
 
-      {/* Work Process Section */}
-      <WorkProcess />
-    </div>
-  );
+			{/* Work Process Section */}
+			<WorkProcess />
+		</div>
+	);
 };
 
 export default About;
